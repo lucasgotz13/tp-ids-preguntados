794191f82a7895ded19001829d3060f347250b36
Lucas Gotz <tutigotz@gmail.com>
2025-06-15 02:56:10 +0000

    hotfix: Algunos errores de sintaxis arreglados

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,13 +1,4 @@
 const express = require("express");
-const { Pool } = require("pg");
-
-const dbClient = new Pool({
-    user: "postgres",
-    password: "postgres",
-    host: "localhost",
-    port: 5432,
-    database: "preguntados",
-});
 
 const app = express();
 
@@ -16,7 +7,6 @@ const port = 3030;
 app.use(express.json());
 
 const {
-
     getAllPreguntas,
     getPreguntaById,
     createPregunta,
@@ -25,14 +15,8 @@ const {
     deletePregunta,
     createRespuesta,
     getAllRespuestas,
-    getOneResouesta
-
-
+    getOneRespuesta,
 } = require("./scripts/connectidb.js");
-
-
-
-
 
 // Obtener todas las preguntas
 app.get("/api/preguntas/", async (req, res) => {
@@ -45,7 +29,7 @@ app.get("/api/preguntas/", async (req, res) => {
 
 // Obtener una pregunta (usando el id)
 app.get("/api/preguntas/:id", async (req, res) => {
-    const response =await getPreguntaById(req.params.id);
+    const response = await getPreguntaById(req.params.id);
     if (response.length === 0) {
         return res.status(404).json("La pregunta no fue encontrada");
     }
@@ -53,17 +37,20 @@ app.get("/api/preguntas/:id", async (req, res) => {
     return res.status(200).json(response);
 });
 
-
 /*curl --header "Content-Type: application/json" \
-  --request POST \
+  --request POST \ 
   --data '{"pregunta":"Como se llama el profe de la catedra?","dificultad":"facil", 
   "categoria" : "fiuba", "puntos": "10"}' \
   http://localhost:3030/api/preguntas/  */
 
 // Crear una pregunta
 app.post("/api/preguntas/", async (req, res) => {
-   
-    if (req.body.pregunta == null || req.body.dificultad == null || req.body.categoria == null || req.body.puntos == null) {
+    if (
+        req.body.pregunta == null ||
+        req.body.dificultad == null ||
+        req.body.categoria == null ||
+        req.body.puntos == null
+    ) {
         return res.status(400).send("Faltan datos para crear la pregunta");
     }
 
@@ -74,7 +61,7 @@ app.post("/api/preguntas/", async (req, res) => {
             req.body.categoria,
             req.body.puntos
         );
-        
+
         return res.status(201).json({
             status: true,
             mensaje: "Pregunta creada exitosamente",
@@ -87,21 +74,19 @@ app.post("/api/preguntas/", async (req, res) => {
 });
 
 // Ver todas las respuestas de todas las preguntas
-app.get("/api/respuestas/", async (req,res) => {
-
+app.get("/api/respuestas/", async (req, res) => {
     const response = await getAllRespuestas();
-    
+
     if (response.length === 0) {
         return res.status(404).json("No hay respuestas");
     }
 
     return res.status(200).json(response);
-}) 
+});
 
 // Ver una respuesta de una pregunta (usando el id)
 app.get("/api/respuestas/:id", async (req, res) => {
-
-    const response = await getOneResouesta(req.params.id);
+    const response = await getOneRespuesta(req.params.id);
 
     if (response.length === 0) {
         return res.status(404).json("La respuesta no fue encontrada");
@@ -110,24 +95,21 @@ app.get("/api/respuestas/:id", async (req, res) => {
     return res.status(200).json(response);
 });
 
-
-
-
 /*curl --header "Content-Type: application/json" \
   --request POST \
   --data '{"id_pregunta":"1","respuesta_a":"Manuel Bilbao", "respuesta_b":"Nico Riedel",
   "respuesta_c" : "La peke", "respuesta_correcta": "Manuel Camejo"}' \
   http://localhost:3030/api/respuestas/  */
 
-
 // crear respuestas de pregunta
 app.post("/api/respuestas/", async (req, res) => {
-
-    if( req.body.id_pregunta == null || 
-        req.body.respuesta_a == null || 
-        req.body.respuesta_b == null || 
-        req.body.respuesta_c == null || 
-        req.body.respuesta_correcta == null) {
+    if (
+        req.body.id_pregunta == null ||
+        req.body.respuesta_a == null ||
+        req.body.respuesta_b == null ||
+        req.body.respuesta_c == null ||
+        req.body.respuesta_correcta == null
+    ) {
         return res.status(400).send("Faltan datos para crear la respuesta");
     }
 
@@ -149,15 +131,16 @@ app.post("/api/respuestas/", async (req, res) => {
             .status(400)
             .json({ status: true, mensaje: "No se pudo crear la respuesta" });
     }
-   
 });
-
-
 
 // Modificar la pregunta
 app.put("/api/preguntas/:id", async (req, res) => {
-   
-if (req.body.pregunta == null || req.body.dificultad == null || req.body.categoria == null || req.body.puntos == null) {
+    if (
+        req.body.pregunta == null ||
+        req.body.dificultad == null ||
+        req.body.categoria == null ||
+        req.body.puntos == null
+    ) {
         return res.status(400).send("Faltan datos para modificar la pregunta");
     }
 
@@ -184,9 +167,7 @@ if (req.body.pregunta == null || req.body.dificultad == null || req.body.categor
 
 // Borrar la pregunta
 app.delete("/api/preguntas/:id", async (req, res) => {
-
-
-    if (req.params,id == null) {
+    if (req.params.id == null) {
         return res.status(400).send("No se mando un id");
     }
 
@@ -204,11 +185,8 @@ app.delete("/api/preguntas/:id", async (req, res) => {
     }
 });
 
-
 // Obtener un solo usuario (usando el id)
 app.get("/api/usuarios/:id", async (req, res) => {
-
-    
     const response = await getOneUser(req.params.id);
 
     if (response.length == 0) {
--- backend/src/scripts/connectidb.js
+++ backend/src/scripts/connectidb.js
@@ -8,7 +8,6 @@ const dbClient = new Pool({
     database: "preguntados",
 });
 
-
 async function getAllPreguntas() {
     const response = await dbClient.query("SELECT * FROM preguntas");
     return response.rows;
@@ -25,62 +24,55 @@ async function getPreguntaById(id) {
 
 async function createPregunta(pregunta, dificultad, categoria, puntos) {
     dbClient.query(
-            "INSERT INTO preguntas (pregunta, dificultad, categoria, puntos) VALUES ($1, $2, $3, $4)",
-            [pregunta, dificultad, categoria, puntos]
-        );
-
+        "INSERT INTO preguntas (pregunta, dificultad, categoria, puntos) VALUES ($1, $2, $3, $4)",
+        [pregunta, dificultad, categoria, puntos]
+    );
 }
 
 async function updatePregunta(id, pregunta, dificultad, categoria, puntos) {
-        dbClient.query(
-            "UPDATE preguntas SET pregunta = $2, dificultad = $3, categoria = $4, puntos = $5 WHERE id = $1",
-            [id, pregunta, dificultad, categoria, puntos]
-        );
-
+    dbClient.query(
+        "UPDATE preguntas SET pregunta = $2, dificultad = $3, categoria = $4, puntos = $5 WHERE id = $1",
+        [id, pregunta, dificultad, categoria, puntos]
+    );
 }
-async function getOneUser(id){
+async function getOneUser(id) {
     const response = await dbClient.query(
-        "SELECT * FROM users WHERE id = $1",
+        "SELECT * FROM usuarios WHERE id = $1",
         [id]
     );
 
-return response.rows;
+    return response.rows;
 }
 
 async function deletePregunta(id) {
     dbClient.query("DELETE FROM preguntas WHERE id = $1", [id]);
-
 }
 
 async function getAllRespuestas() {
-
-    const response = await dbClient.query(
-        "SELECT * FROM respuestas"
-    );
+    const response = await dbClient.query("SELECT * FROM respuestas");
     return response.rows;
-
 }
 
-async function createRespuesta(id_pregunta, respuesta_a, respuesta_b, respuesta_c, respuesta_correcta) {
-
+async function createRespuesta(
+    id_pregunta,
+    respuesta_a,
+    respuesta_b,
+    respuesta_c,
+    respuesta_correcta
+) {
     dbClient.query(
         "INSERT INTO respuestas (id_pregunta, respuesta_a, respuesta_b, respuesta_c, respuesta_correcta) VALUES ($1, $2, $3, $4, $5)",
         [id_pregunta, respuesta_a, respuesta_b, respuesta_c, respuesta_correcta]
     );
-
-
 }
-async function getOneRespuesta(id){
+async function getOneRespuesta(id) {
     const response = await dbClient.query(
         "SELECT * FROM respuestas WHERE id = $1",
         [id]
     );
 
     return response.rows;
-
-
 }
-
 
 module.exports = {
     getAllPreguntas,
@@ -91,7 +83,5 @@ module.exports = {
     deletePregunta,
     createRespuesta,
     getAllRespuestas,
-    getOneRespuesta
-
-
+    getOneRespuesta,
 };
